384bb3241409e22bbf48e7c018725f731e08a5f8
tusharsanjay <tusharduratkar12@gmail.com>
2025-12-30 17:45:47 +0530

    added ML integration

---
+++ training/train_models.py
@@ -0,0 +1,13 @@
+import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+
+df = (
+    pd.
+    read_csv('car-details.csv')
+)
